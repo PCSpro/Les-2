c28ba3fa7e623412cdc2d8a26cd061314056faeb
PCSpro <drago110103@gmail.com>
2024-12-10 18:53:17 +0500

    Add files via upload

---
+++ Функция с параметром.py
@@ -0,0 +1,10 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append( value)
+    return matrix
+print(get_matrix(3,4,5))
+print(get_matrix(2,5,1))
+print(get_matrix(2,2,0))
